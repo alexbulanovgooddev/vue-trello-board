b4d24a698069c4cdb0fd6c335c006a80ebb33155
alexbulanovgooddev <alexbulanovgooddev@gmail.com>
2026-06-07 18:00:42 +0300

    feat: добавлено удаление карточки по Backspace

--- src/components/TrelloBoard.vue
+++ src/components/TrelloBoard.vue
@@ -78,7 +78,11 @@ const alt = useKeyModifier('Alt')
 					handle=".drag-handle">
 					<template #item="{ element: task }: { element: Task }">
 						<div>
-							<TrelloBoardTask :task="task" />
+							<TrelloBoardTask
+								:task="task"
+								@delete="
+									column.tasks = column.tasks.filter(t => t.id !== $event)
+								" />
 						</div>
 					</template>
 				</draggable>
--- src/components/TrelloBoardTask.vue
+++ src/components/TrelloBoardTask.vue
@@ -1,18 +1,35 @@
 <script setup lang="ts">
-import type { Task } from '@/types'
+import type { ID, Task } from '@/types'
+import { ref } from 'vue'
 import DragHandle from './DragHandle.vue'
+import { onKeyStroke } from '@vueuse/core'
 
 interface Props {
 	task: Task
 }
 
-defineProps<Props>()
+const props = defineProps<Props>()
+
+const emit = defineEmits<{
+	(e: 'delete', payload: ID): void
+}>()
+
+const focused = ref<boolean>(false)
+
+onKeyStroke('Backspace', () => {
+	if (focused.value) {
+		emit('delete', props.task.id)
+	}
+})
 </script>
 
 <template>
 	<div
-		class="task mb-2 flex gap-1 rounded max-w-62.5 p-2 bg-white shadow-sm"
-		:title="task.createdAt.toLocaleDateString()">
+		class="task mb-2 flex gap-1 rounded max-w-62.5 p-2 bg-white shadow-sm focus-visible:outline-gray-900 focus:outline-gray-900"
+		:title="task.createdAt.toLocaleDateString()"
+		tabindex="0"
+		@focus="focused = true"
+		@blur="focused = false">
 		<DragHandle class="mt-1" size="sm" />
 		<span>{{ task.title }}</span>
 	</div>
