d49cc339db889d33eb2bd941046a6b861041aa28
alexbulanovgooddev <alexbulanovgooddev@gmail.com>
2026-08-16 14:58:30 +0300

    feat(board): статичная вёрстка доски
    - добавлен компонент TrelloBoard с колонками и карточками задач
    - моковые данные колонок с id через nanoid
    - в App.vue добавлены шапка с логотипом и подключение доски

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,16 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import TrelloBoard from './components/TrelloBoard.vue'
+</script>
 
 <template>
-	<div class="flex">HelloWorld</div>
+	<div class="flex flex-col h-svh py-10 bg-teal-600 overflow-auto">
+		<div class="mb-4 px-10">
+			<div class="flex items-center gap-2">
+				<img class="w-9 h-9" src="./assets/vite.svg" alt="" />
+				<h1 class="text-3xl font-bold font-mono text-white">TrelloBoard</h1>
+			</div>
+		</div>
+
+		<TrelloBoard />
+	</div>
 </template>
---
+++ src/components/TrelloBoard.vue
@@ -0,0 +1,74 @@
+<script setup lang="ts">
+import type { Column } from '@/types'
+
+import { ref } from 'vue'
+import { nanoid } from 'nanoid'
+
+const columns = ref<Column[]>([
+	{
+		id: nanoid(),
+		title: 'Backlog',
+		tasks: [
+			{
+				id: nanoid(),
+				title: 'Create marketing landing page',
+				createdAt: new Date()
+			},
+			{
+				id: nanoid(),
+				title: 'Develop cool new feature',
+				createdAt: new Date()
+			},
+			{
+				id: nanoid(),
+				title: 'Fix page nav bug',
+				createdAt: new Date()
+			}
+		]
+	},
+	{
+		id: nanoid(),
+		title: 'Selected for Dev',
+		tasks: []
+	},
+	{
+		id: nanoid(),
+		title: 'In Progress',
+		tasks: []
+	},
+	{
+		id: nanoid(),
+		title: 'QA',
+		tasks: []
+	},
+	{
+		id: nanoid(),
+		title: 'Complete',
+		tasks: []
+	}
+])
+</script>
+
+<template>
+	<div class="px-10 py-1 flex-1 flex items-start gap-4 overflow-x-auto">
+		<div class="flex items-start gap-4">
+			<div
+				v-for="column in columns"
+				:key="column.id"
+				class="rounded min-w-62.5 bg-gray-200 p-5">
+				<div class="mb-4 flex items-center gap-1">
+					{{ column.title }}
+				</div>
+
+				<div>
+					<div
+						v-for="task in column.tasks"
+						:key="task.id"
+						class="mb-2 flex gap-1 rounded max-w-62.5 p-2 bg-white shadow-sm">
+						<span>{{ task.title }}</span>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</template>
